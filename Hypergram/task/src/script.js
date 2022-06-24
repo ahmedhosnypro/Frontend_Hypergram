2f1dec06edfd29f571f4ee1123a1e537f6173298
ahmedhosnypro <ahhosnyas@gmail.com>
2022-06-24 21:42:31 +0200

    First steps

---
+++ Hypergram/task/src/script.js
@@ -0,0 +1,9 @@
+let uploadButton = document.getElementById('upload-button');
+let inputFile = document.getElementById('file-input');
+let imageArea = document.getElementById('img-canvas');
+
+uploadButton.addEventListener('click', uploadImage);
+
+function uploadImage() {
+
+}
