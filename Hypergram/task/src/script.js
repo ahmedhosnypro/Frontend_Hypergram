f3e1d686b19b2d01fe21f03fe13df4894098c06f
ahmedhosnypro <ahhosnyas@gmail.com>
2022-06-24 22:59:23 +0200

    What a landscape

--- Hypergram/task/src/script.js
+++ Hypergram/task/src/script.js
@@ -1,9 +1,26 @@
-let uploadButton = document.getElementById('upload-button');
-let inputFile = document.getElementById('file-input');
-let imageArea = document.getElementById('img-canvas');
+const fileInput = document.getElementById('file-input');
+const imageContainer = document.getElementById('image-container');
 
-uploadButton.addEventListener('click', uploadImage);
+fileInput.addEventListener('change',
+    function (ev) {
+        console.log("hi");
+        console.log(ev.target.files);
+        if (ev.target.files) {
+            let file = ev.target.files[0];
+            const reader = new FileReader();
 
-function uploadImage() {
-
-}
+            reader.onloadend = function (e) {
+                const image = new Image();
+                image.src = e.target.result;
+                image.onload = function () {
+                    console.log("loading");
+                    const canvas = document.getElementById('canvas');
+                    canvas.width = image.width;
+                    canvas.height = image.height;
+                    const ctx = canvas.getContext('2d');
+                    ctx.drawImage(image, 0, 0);
+                }
+            }
+            reader.readAsDataURL(file);
+        }
+    })
