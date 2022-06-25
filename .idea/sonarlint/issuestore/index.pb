97b7dbf341b8c50af86eca8704bd4d219e8fb7b2
ahmedhosnypro <ahhosnyas@gmail.com>
2022-06-25 04:38:05 +0200

    A tiny pixel

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+M
+Hypergram/task/src/camera.svg,2\a\2adf1e25afae3f6d58c389a92ec60d6664fba343
+Y
+)Hypergram/What a landscape/task-info.yaml,9\d\9df60b319cc88bca9626864333640da2bf357522
+T
+$Hypergram/First steps/task-info.yaml,e\b\ebfb26c398d94871ebc00c99c70869af779406f0
--- Hypergram/task/src/script.js
+++ Hypergram/task/src/script.js
@@ -1,26 +1,75 @@
 const fileInput = document.getElementById('file-input');
-const imageContainer = document.getElementById('image-container');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const brightnessSlider = document.getElementById('brightness');
+const contrastSlider = document.getElementById('contrast');
+const transparencySlider = document.getElementById('transparent');
+const image = new Image()
 
 fileInput.addEventListener('change',
-    function (ev) {
-        console.log("hi");
-        console.log(ev.target.files);
-        if (ev.target.files) {
-            let file = ev.target.files[0];
+    (event) => {
+        if (event.target.files) {
+            const file = event.target.files[0];
             const reader = new FileReader();
-
-            reader.onloadend = function (e) {
-                const image = new Image();
+            reader.readAsDataURL(file);
+            reader.onloadend = (e) => {
                 image.src = e.target.result;
-                image.onload = function () {
-                    console.log("loading");
-                    const canvas = document.getElementById('canvas');
+                image.onload = () => {
                     canvas.width = image.width;
                     canvas.height = image.height;
-                    const ctx = canvas.getContext('2d');
                     ctx.drawImage(image, 0, 0);
                 }
             }
-            reader.readAsDataURL(file);
+
         }
     })
+
+brightnessSlider.addEventListener('change', () => updateImage())
+contrastSlider.addEventListener('change', () => updateImage())
+transparencySlider.addEventListener('change', () => updateImage())
+
+function updateImage() {
+    const brightness = parseInt(brightnessSlider.value)
+    const contrast = parseInt(contrastSlider.value)
+    const transparency = parseFloat(transparencySlider.value)
+
+    ctx.drawImage(image, 0, 0)
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    const pixels = imageData.data
+
+    const factor = 259 * (255 + contrast) / (255 * (259 - contrast))
+    pixels.forEach((pixel, index) => {
+        if (index % 4 === 0) {
+            pixels[index] = truncate(factor * (pixel - 128) + 128 + brightness)
+        }
+        if (index % 4 === 1) {
+            pixels[index] = truncate(factor * (pixel - 128) + 128 + brightness)
+        }
+        if (index % 4 === 2) {
+            pixels[index] = truncate(factor * (pixel - 128) + 128 + brightness)
+        }
+    })
+
+    if (transparency !== 1) {
+        pixels.forEach((pixel, index) => {
+            if (index % 4 === 3) {
+                pixels[index] = pixel * transparency
+            }
+        })
+        pixels.forEach((pixel, index) => {
+            pixels[index] = truncate(pixel)
+        })
+    }
+    imageData.data = pixels
+    ctx.putImageData(imageData, 0, 0)
+}
+
+function truncate(value) {
+    if (value < 0) {
+        return 0
+    } else if (value > 255) {
+        return 255
+    } else {
+        return value
+    }
+}
