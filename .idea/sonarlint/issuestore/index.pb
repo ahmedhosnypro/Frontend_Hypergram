4b10c5af058d8fc211627f0cf00f8f428903a881
ahmedhosnypro <ahhosnyas@gmail.com>
2022-06-25 05:02:10 +0200

    Download the result

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,5 @@
 
 M
 Hypergram/task/src/camera.svg,2\a\2adf1e25afae3f6d58c389a92ec60d6664fba343
-Y
-)Hypergram/What a landscape/task-info.yaml,9\d\9df60b319cc88bca9626864333640da2bf357522
-T
-$Hypergram/First steps/task-info.yaml,e\b\ebfb26c398d94871ebc00c99c70869af779406f0
+L
+Hypergram/task/src/style.css,5\a\5aeec1cade37249d605550b6d77c0aa298d339d8
--- Hypergram/task/src/script.js
+++ Hypergram/task/src/script.js
@@ -1,9 +1,12 @@
 const fileInput = document.getElementById('file-input');
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+
 const brightnessSlider = document.getElementById('brightness');
 const contrastSlider = document.getElementById('contrast');
 const transparencySlider = document.getElementById('transparent');
+const saveImageButton = document.getElementById('save-button');
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 const image = new Image()
 
 fileInput.addEventListener('change',
@@ -22,54 +25,71 @@ fileInput.addEventListener('change',
             }
 
         }
-    })
+    });
 
-brightnessSlider.addEventListener('change', () => updateImage())
-contrastSlider.addEventListener('change', () => updateImage())
-transparencySlider.addEventListener('change', () => updateImage())
+brightnessSlider.addEventListener('change', () => updateImage());
+contrastSlider.addEventListener('change', () => updateImage());
+transparencySlider.addEventListener('change', () => updateImage());
+saveImageButton.addEventListener("click", ()=>{
+    downloadImage()
+});
 
 function updateImage() {
-    const brightness = parseInt(brightnessSlider.value)
-    const contrast = parseInt(contrastSlider.value)
-    const transparency = parseFloat(transparencySlider.value)
+    const brightness = parseInt(brightnessSlider.value);
+    const contrast = parseInt(contrastSlider.value);
+    const transparency = parseFloat(transparencySlider.value);
 
-    ctx.drawImage(image, 0, 0)
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
-    const pixels = imageData.data
+    ctx.drawImage(image, 0, 0);
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const pixels = imageData.data;
 
-    const factor = 259 * (255 + contrast) / (255 * (259 - contrast))
+    const factor = 259 * (255 + contrast) / (255 * (259 - contrast));
     pixels.forEach((pixel, index) => {
         if (index % 4 === 0) {
-            pixels[index] = truncate(factor * (pixel - 128) + 128 + brightness)
+            pixels[index] = truncate(factor * (pixel - 128) + 128 + brightness);
         }
         if (index % 4 === 1) {
-            pixels[index] = truncate(factor * (pixel - 128) + 128 + brightness)
+            pixels[index] = truncate(factor * (pixel - 128) + 128 + brightness);
         }
         if (index % 4 === 2) {
-            pixels[index] = truncate(factor * (pixel - 128) + 128 + brightness)
+            pixels[index] = truncate(factor * (pixel - 128) + 128 + brightness);
         }
     })
 
     if (transparency !== 1) {
         pixels.forEach((pixel, index) => {
             if (index % 4 === 3) {
-                pixels[index] = pixel * transparency
+                pixels[index] = pixel * transparency;
             }
         })
         pixels.forEach((pixel, index) => {
-            pixels[index] = truncate(pixel)
+            pixels[index] = truncate(pixel);
         })
     }
-    imageData.data = pixels
-    ctx.putImageData(imageData, 0, 0)
+    imageData.data = pixels;
+    ctx.putImageData(imageData, 0, 0);
 }
 
 function truncate(value) {
     if (value < 0) {
-        return 0
+        return 0;
     } else if (value > 255) {
-        return 255
+        return 255;
     } else {
-        return value
+        return value;
     }
+}
+
+
+function downloadImage() {
+    console.log("download");
+    
+    const imageData = canvas.toDataURL();
+    let tmpLink = document.createElement('a');
+    tmpLink.download = 'result.png';
+    tmpLink.href = imageData;
+
+    document.body.appendChild( tmpLink );
+    tmpLink.click();
+    document.body.removeChild( tmpLink );
 }
